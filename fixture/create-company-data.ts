b2812640378e1b9b96e101d248f44d741d5fe21b
peru1993 <peru.caliaperumal@twilightitsolutions.com>
2025-02-08 17:54:31 +0530

    test: gp2-188/189

---
+++ fixture/create-company-data.ts
@@ -0,0 +1,13 @@
+export const createCompanyValidData = {
+    companyName: 'HealthPointer NHF Group',
+    streetAddress1: '987 Atlantic Avenue',
+    streetAddress2: 'Avenur Park',
+    city: 'Virginia Beach',
+    state: 'Virginia(VA)',
+    Countryname: 'United States of America',
+    zipCode: '23451',
+    phoneNumber: '7575551212',
+    mobileNumber: '2765551212',
+    websiteAddress: 'https://healthpointernhfgroup.com/',
+    domainaddress: 'HealthPointer',
+  };
